46f26b70aba81f19d66d3b05aa458c076f7e29ed
Valeri Sabev <valchygaming@gmail.com>
2022-06-06 18:26:30 +0200

    Code splitted routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import "./styles.css";
-import HomePage from "./scenes/HomePage";
-import VideoGame from "./scenes/VideoGame";
-import { Routes, Route } from "react-router-dom";
+import './styles.css';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const LazyHomePage = lazy(() => import('./scenes/HomePage'));
+const LazyVideoGame = lazy(() => import('./scenes/VideoGame'));
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/game" element={<VideoGame />} />
-    </Routes>
-  );
+	return (
+		<Suspense fallback={null}>
+			<Routes>
+				<Route path="/" element={<LazyHomePage />} />
+				<Route path="/game" element={<LazyVideoGame />} />
+			</Routes>
+		</Suspense>
+	);
 }
